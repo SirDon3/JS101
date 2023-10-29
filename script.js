94ec06c1f82216d7309f79fbda6bb11fe5069a7e
SirDon3 <110694491+SirDon3@users.noreply.github.com>
2023-10-29 17:33:07 +0200

    05_04

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-const mainLi = document.querySelector("main li");
+function trySomething (a, b){
+    let c = a + b;
+    return c
+} 
 
-console.log("This the main li:", mainLi);
+function getIteam (name) {
 
-document.querySelectorAll("main li").forEach(iteam => iteam.style.backgroundColor = "pink");
+    let newClass = name;
+    let viewIteam = document.querySelector("main").classList.add(newClass);
+    return viewIteam
 
-console.log("Color changed!");
+}
 
-document.querySelectorAll("main li:last-child").forEach(iteam => iteam.style.backgroundColor = "red");
+const newMathsTry = function (a, b){
 
-console.log("Color changed for all last child!");
+    let c = a * b;
+
+    return c;
+
+
+}
+console.log("Lets do simple Maths:", trySomething(6, 6))
+getIteam("tryMe")
+console.log("Lets do simple Maths again:", newMathsTry(2, 6))
+console.log(document.querySelector("main"))
+
